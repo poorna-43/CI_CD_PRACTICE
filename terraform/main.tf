a32218ace6e2ed8bd7246c44ebb49d8057431830
ANJURU POORNA CHANDU <poornachanduanjuru1@gmail.com>
2026-02-04 12:05:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_s3_bucket" "site" {
+  bucket = "your-unique-bucket-name"
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
